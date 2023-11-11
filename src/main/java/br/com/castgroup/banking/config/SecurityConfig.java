d34216520c2710d08da70a09ff734a210aefc3bc
Marcelo Schambeck <mschambeck@gmail.com>
2023-11-10 23:55:48 -0300

    Keycloak

--- src/main/java/br/com/castgroup/banking/config/SecurityConfig.java
+++ src/main/java/br/com/castgroup/banking/config/SecurityConfig.java
@@ -1,18 +1,24 @@
 package br.com.castgroup.banking.config;
 
-//@Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
 class SecurityConfig {
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(withDefaults())
-//                .cors(withDefaults())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/swagger-resources", "/v3/api-docs/**", "/proxy/**").permitAll()
-//                        .anyRequest().authenticated()
-//                        .anyRequest().permitAll()
-//                )
-//                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(withDefaults()));
-//        return http.build();
-//    }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(withDefaults())
+                .cors(withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/swagger-resources", "/v3/api-docs/**", "/proxy/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(withDefaults()));
+        return http.build();
+    }
 }
